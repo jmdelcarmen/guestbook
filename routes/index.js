a818a3113d458ffbdda1d1991d339c488ffdfdef
Marco <jmdc@my.codergv.com>
2016-10-26 20:20:07 -0500

    Added UI

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,58 @@
+'use strict';
 var express = require('express');
 var ObjectID = require('mongodb').ObjectID;
 var router = express.Router();
 
-//Guestlist
-router.get('/', function (req, res) {
-  var db = req.db;
-  var collection = db.get('guests');
-  collection.find({}, {}, function (e, docs) {
-    res.render('index', {
-      guestlist: docs,
-      "title": "Guestbook"
+
+
+router.route('/')
+  //Guestlist
+  .get((req, res) => {
+    var db = req.db;
+    var collection = db.get('guests');
+    collection.find({}, {}, function (e, docs) {
+      res.render('index', {
+        guestlist: docs,
+        "title": "Guestbook"
+      });
     });
-  });
+  })
+  //Add guest to guestlist
+  .post((req, res) => {
+    //Set out internal DB variable
+    var db = req.db;
+    //Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+    var userMessage = req.body.usermessage;
+    //Set our collection
+    var collection = db.get('guests');
+
+    //Submit to the db
+    if (userName !== "" && userEmail !== "" && userMessage !== "") {
+      collection.insert({
+        "username": userName,
+        "email": userEmail,
+        "message": userMessage,
+        "date": new Date().toDateString()
+      }, (err, doc) => {
+          if (err) {
+            //If it failed, return error
+            res.send('There was a problem adding the information to the database.');
+          }
+          else {
+            //And forward to success page
+            res.redirect('/');
+          }
+      });
+    }
+    else {
+      res.redirect('/');
+    }
 });
+
+
+
 
 //Direct to page with guest information
 router.get('/:username', function (req, res){
@@ -40,39 +80,7 @@ router.get('/:id/delete/', function (req, res) {
 
 
 
-//Add guest to guestlist
-router.post('/', (req, res) => {
-  //Set out internal DB variable
-  var db = req.db;
-  //Get our form values. These rely on the "name" attributes
-  var userName = req.body.username;
-  var userEmail = req.body.useremail;
-  var userMessage = req.body.usermessage;
-  //Set our collection
-  var collection = db.get('guests');
 
-  //Submit to the db
-  if (userName !== "" && userEmail !== "" && userMessage !== "") {
-    collection.insert({
-      "username": userName,
-      "email": userEmail,
-      "message": userMessage,
-      "date": new Date().toDateString()
-    }, (err, doc) => {
-        if (err) {
-          //If it failed, return error
-          res.send('There was a problem adding the information to the database.');
-        }
-        else {
-          //And forward to success page
-          res.redirect('/');
-        }
-    });
-  }
-  else {
-    res.redirect('/');
-  }
-});
 
 
 module.exports = router;
